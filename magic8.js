e99daef0a8e7c3ebf278c376af0e980f1138e75f
Tsantos <trixiea007@gmail.com>
2022-09-02 22:37:22 -0700

    magic8 ball js

---
+++ magic8.js
@@ -0,0 +1,41 @@
+let userName = 'Trixie';
+
+userName ? console.log(`Hello, ${userName}`) : console.log('Hello!');
+
+let question = 'When I get to go on a vacation soon?';
+
+console.log(`\n${userName}'s question is '`+ question);
+
+let randomNumber = Math.floor(Math.random() * 8);
+//console.log(randomNumber);
+
+let eightBall = '';
+
+switch(randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log('\n' + eightBall);
